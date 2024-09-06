21182aa5c948e18a66c1dd23cb378e5c248ad8e8
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-06 13:24:45 +0900

    Update Wall.java

--- src/Wall.java
+++ src/Wall.java
@@ -1,4 +1,14 @@
 package PACKAGE_NAME;
 
 public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
+
